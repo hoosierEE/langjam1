381b30bbd8f7c32db3ddd5849fe2e9d5d50ab953
Alex Shroyer <alex@cato.digital>
2025-06-18 09:21:38 -0400

    debugging

--- src/repl.ex
+++ src/repl.ex
@@ -1,16 +1,21 @@
+
 defmodule Parser do
+  def e1, do: "[]" |> parse
+  def e2, do: "[1 2 3]" |> parse
+  def e3, do: "1 [2 3]" |> parse
   def tokenize(line), do: Regex.scan(~r/-?\d+|[\[\]\{\}\(\)\+\-\*]|\w+/, line) |> List.flatten()
   def parse(str) when is_binary(str), do: str |> tokenize |> parse
   def parse(tokens), do: do_parse(tokens, Stack.new())
   defp do_parse([], stack), do: {:expr, Enum.reverse(stack)}
   defp do_parse(["[" | rest], stack) do
     {nested_items, rest2} = extract_list(rest, [])
-    {:list, items} = parse_list(nested_items)
-    stack = Stack.push(stack, {:list, items})
+    {:lst, items} = parse_list(nested_items)
+    stack = Stack.push(stack, {:lst, items})
     do_parse(rest2, stack)
   end
   defp do_parse([token | rest], stack), do: do_parse(rest, Stack.push(stack, token))
-  # [tokens] => {:list, [tokens]}
+
+  # [tokens] => {:lst, [tokens]}
   defp extract_list([], _), do: raise "Unmatched ["
   defp extract_list(["[" | rest], acc) do
     {nested, rest2} = extract_list(rest, [])
@@ -18,9 +23,10 @@ defmodule Parser do
   end
   defp extract_list(["]" | rest], acc), do: {Enum.reverse(acc), rest}
   defp extract_list([token | rest], acc), do: extract_list(rest, [token | acc])
+
   # flatten [:open | nested] markers into nested lists
   defp parse_list(tokens) do
-    {:list, Enum.map(tokens, fn
+    {:lst, Enum.map(tokens, fn
         [:open | nested_tokens] -> parse_list(nested_tokens)
         token -> token
       end)}
@@ -30,13 +36,11 @@ end
 
 defmodule AstEvaluator do
   def eval({:expr, nodes}), do: eval_nodes(nodes, [])
-
   defp eval_nodes([], [result]), do: result
-  defp eval_nodes([], stack), do: raise "Unexpected stack at end: #{inspect(stack)}"
-
+  defp eval_nodes([], stack), do: raise "Unexpected stack at end: #{inspect(stack, charlists: :as_lists)}"
   defp eval_nodes([node | rest], stack) do
     case node do
-      {:list, items} ->
+      {:lst, items} ->
         value = eval({:expr, items})
         eval_nodes(rest, [value | stack])
 
@@ -80,59 +84,7 @@ defmodule AstEvaluator do
   end
 end
 
-
-# defmodule AstEval do
-#   def eval({:expr, nodes}), do: eval_nodes(nodes, [])
-
-#   defp eval_nodes([], [result]) do
-#     result
-#   end
-#   defp eval_nodes([], stack), do: raise "Unexpected stack at end: #{inspect(stack)}"
-#   defp eval_nodes([node | rest], stack) do
-#     case node do
-#       {:list, items} ->
-#         value = eval({:expr, items})
-#         IO.inspect(value)
-#         eval_nodes(rest, [value | stack])
-#       op when op in ~w"+ - * /" ->
-#         [b, a | stack_rest] = stack
-#         result = apply_op(op, a, b)
-#         eval_nodes(rest, [result | stack_rest])
-#       value ->
-#         val = parse_value(value)
-#         eval_nodes(rest, [val | stack])
-#     end
-#   end
-
-#   defp apply_op(op, a, b) do
-#     fun = op_fn(op)
-#     cond do
-#       is_list(a) and is_list(b) -> Enum.zip_with(a, b, fun)
-#       is_list(a)                -> Enum.map(a, &fun.(&1, b))
-#       is_list(b)                -> Enum.map(b, &fun.(&1, a))
-#       true                      -> fun.(a, b)
-#     end
-#   end
-#   defp op_fn("+"), do: &+/2
-#   defp op_fn("-"), do: &-/2
-#   defp op_fn("*"), do: &*/2
-#   defp op_fn("/"), do: &div/2
-
-#   defp parse_value(val) when is_integer(val), do: val
-#   defp parse_value(val) when is_list(val), do: Enum.map(val, &parse_value/1)
-#   defp parse_value(val) when is_binary(val) do
-#     case Integer.parse(val) do
-#       {int, ""} -> int
-#       _ -> val
-#     end
-#   end
-# end
-
 defmodule Stack do
   def new, do: []
   def push(stack, value), do: [value | stack]
-  # def pop([top | rest]), do: {top, rest}
-  # def pop([]), do: {:error, :empty_stack}
-  # def peek([top | _]), do: top
-  # def peek([]), do: {:error, :empty_stack}
 end
